35790a855c4e450c5fd5261180ae83595ff6e5e1
extraaedge <shekharb@theextraaedge.com>
2017-09-22 20:34:32 +0530

    Commit_2209_I

--- EE_Web/src/whitefeather/xedge/core/Page_GlobalSearch.java
+++ EE_Web/src/whitefeather/xedge/core/Page_GlobalSearch.java
@@ -1,16 +1,24 @@
 package whitefeather.xedge.core;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
 
 import whitefeather.xedge.appconfig.Constants;
 import whitefeather.xedge.appconfig.ObjectMapping;
 import whitefeather.xedge.facilitator.HelperHand;
+import whitefeather.xedge.testcases.Refresh_Page;
 
 public class Page_GlobalSearch extends HelperHand
 {
 	public static WebElement element;
-	
+	public static String elementText="",validator="",noResult="",nullResult="";
 	public static ObjectMapping properties = new ObjectMapping(Constants.LOCATORS);	
+	public static final String prospectEmailCase = (DataGenerator.prospectEmail).toString();
 	
 	public static boolean displayGlobalSearchBar()
 	{
@@ -50,11 +58,35 @@ public class Page_GlobalSearch extends HelperHand
 		return element;
 	}
 	
-	public static WebElement displayNoResultMessage()
+	public static WebElement displayResultPageText()
+	{
+		try {
+//			element = driver.findElement(properties.selectLocator("SearchResult_TextLabel"));
+			Thread.sleep(2000);
+			element = driver.findElement(properties.selectLocator("SearchValidator_HTMLElement"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return element;
+	}
+	
+	/*public static WebElement displayNoResultMessage()
 	{
 		try {
 			element = driver.findElement(properties.selectLocator("NoResultMessage_GlobalSearch"));
+			if(element.isDisplayed())
+			{
+				noResult = element.getText();
+			}
+			else
+			{
+				noResult = element.getText();
+			}
+			
+		} catch (NoSuchElementException e) {
+			e.printStackTrace();
 		} catch (Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return element;
@@ -64,10 +96,144 @@ public class Page_GlobalSearch extends HelperHand
 	{
 		try {
 			element = driver.findElement(properties.selectLocator("NullResultMessage_GlobalSearch"));
+			if(element.isDisplayed())
+			{
+				nullResult = element.getText();
+			}
+			else
+			{
+				nullResult = element.getText();
+			}
+			
+		} catch (NoSuchElementException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return element;
+	}*/
+	
+	public static WebElement displaySuccessResult()
+	{
+		try {
+			element = driver.findElement(properties.selectLocator("GlobalSearch_SuccessResult"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return element;
 	}
+	public static String getElementText(WebElement ele)
+	{
+		try {
+			elementText = ele.getText();
+		} catch (NoSuchElementException e) {
+		}
+		return elementText;
+	}
+
 	
+	public static WebElement knowErrorMessage() throws InterruptedException
+	{
+		
+			if(displayResultPageText().getText().contains("Oops"))
+			{
+				Thread.sleep(2000);
+				noResult = displayResultPageText().getText();
+				visibleFlag = false;
+			}
+			else
+			{
+				Thread.sleep(2000);
+				nullResult = displayResultPageText().getText();
+				visibleFlag = false;
+			}
+			if(!displayResultPageText().isDisplayed())
+			{
+				Thread.sleep(2000);
+				visibleFlag = true;
+			}
+			
+		return element;
+	}
+	
+	
+	
+	public static boolean validateGlobalSearch(String validatorString) throws Exception
+	{		
+		String errormsg="";
+//		 String prospectEmailCase = (DataGenerator.prospectEmail).toString();
+		 // prospectEmailCase.toString();
+		errormsg = validatorString;			
+		try 
+		{	
+			if(noResult.contains("Oops"))
+			{
+				errormsg = noResult;	
+			}
+			else if(nullResult.contains("enter"))
+			{
+				errormsg = nullResult;
+			}
+			else if(visibleFlag)
+			{
+				errormsg = DataGenerator.prospectEmail;
+			}
+			
+			switch (errormsg) 
+			{
+			
+			case "Oops! No data found! Please try again.":
+
+				try 
+				{
+//					System.out.println("No Result Found.");
+					HelperHand.getscreenshot(helperString); 
+					Reporter.log("Screenshot is taken and stored at specified location successfully.", true); 
+					Reporter.log("No results found for provided string.", true); 
+				} catch (Exception e) {
+					System.out.println("Exception Handled for No Result.");
+					e.printStackTrace();
+				}
+				break;
+			case "Please Enter Value":	
+
+				try 
+				{
+//					System.out.println("No string is provided.");
+					HelperHand.getscreenshot(helperString); 
+					Reporter.log("Screenshot is taken and stored at specified location successfully.", true); 
+					Reporter.log("User has not provided any string to search.", true); 
+				} catch (Exception e) {
+					System.out.println("Exception Handled for Null Result.");
+					e.printStackTrace();
+				}
+				break;
+			
+			case "":
+				try {
+					System.out.println(prospectEmailCase);
+//					System.out.println("Result Found.");
+					Reporter.log("Specified user details are successfully located in the system.", true); 
+					HelperHand.getscreenshot(helperString);				 
+					Reporter.log("Screenshot is taken and stored at specified location successfully.", true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				break;
+				
+			default:
+
+				try {
+					System.out.println("No global search test case has matched the criteria.");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				break;
+			}
+		} catch (NoSuchElementException e) 
+		{
+			System.out.println(e);
+		}
+		return visibleFlag;
+	}
 }
--- EE_Web/src/whitefeather/xedge/testcases/GlobalSearch.java
+++ EE_Web/src/whitefeather/xedge/testcases/GlobalSearch.java
@@ -65,77 +65,218 @@ public class GlobalSearch extends HelperHand
 			Reporter.log("Global Search Go button is not displayed.", true);
 		}
 	}
-	
-	@Test
-	public void performGlobalSearch() throws Exception
-	{
-		helperString = Thread.currentThread().getStackTrace()[1].getMethodName();
-		Thread.sleep(2000);
-			try {
-				if(Page_GlobalSearch.displayPlaceHolder().isDisplayed())
-					{
-					Page_GlobalSearch.displayPlaceHolder().sendKeys(DataGenerator.prospectEmail+"s");
-					Thread.sleep(1000);
-					Page_GlobalSearch.displayGoButton().click();
+
+	/*@Test 
+ 	public void performGlobalSearch() throws Exception 
+ 	{ 
+ 		helperString = Thread.currentThread().getStackTrace()[1].getMethodName(); 
+ 		Thread.sleep(2000); 
+ 			try { 
+				if(Page_GlobalSearch.displayPlaceHolder().isDisplayed()) 
+					{ 
+//					Page_GlobalSearch.displayPlaceHolder().sendKeys(DataGenerator.prospectEmail); 
+					Page_GlobalSearch.displayPlaceHolder().sendKeys("f176pqvmbs@domain.in"); 
+					Thread.sleep(1000); 
+					Page_GlobalSearch.displayGoButton().click(); 
+					 
+					(new WebDriverWait(driver, 10)) 
+//				   .until(ExpectedConditions.elementToBeClickable(By.xpath(".//span[contains(text(),'"+DataGenerator.prospectEmail+"')]"))); 
+					   .until(ExpectedConditions.elementToBeClickable(By.xpath(".//span[contains(text(),'f176pqvmbs@domain.in')]"))); 
+
+//					Assert.assertEquals(DataGenerator.prospectEmail, driver.findElement(By.xpath(".//span[contains(text(),'"+DataGenerator.prospectEmail+"')]")).getText()); 
+					Assert.assertEquals(DataGenerator.prospectEmail, driver.findElement(By.xpath(".//span[contains(text(),'f176pqvmbs@domain.in')]")).getText()); 
+
 					
-					(new WebDriverWait(driver, 10))
-				   .until(ExpectedConditions.elementToBeClickable(By.xpath(".//span[contains(text(),'"+DataGenerator.prospectEmail+"')]")));
-				
-					Assert.assertEquals(DataGenerator.prospectEmail, driver.findElement(By.xpath(".//span[contains(text(),'"+DataGenerator.prospectEmail+"')]")).getText());
-				
-					Reporter.log("Specified user details are successfully located in the system.", true);
-					HelperHand.getscreenshot(helperString);				
-					Reporter.log("Screenshot is taken and stored at specified location successfully.", true);
-					
-					}
-					else 
-					{
-						Thread.sleep(1000);
-						if(Page_GlobalSearch.displayNoResultMessage().isDisplayed())
-						{
-							HelperHand.getscreenshot(helperString);
-							Reporter.log("Screenshot is taken and stored at specified location successfully.", true);
-							Reporter.log("No results found for provided string.", true);
-						}
-						else
-						{
-							Thread.sleep(1000);
-							if(Page_GlobalSearch.displayNullResultMessage().isDisplayed())
-							{
-								HelperHand.getscreenshot(helperString);
-								Reporter.log("Screenshot is taken and stored at specified location successfully.", true);
-								Reporter.log("User has not provided any string to search.", true);
-							}
-						}	
-					}
+					Reporter.log("Specified user details are successfully located in the system.", true); 
+					HelperHand.getscreenshot(helperString);				 
+					Reporter.log("Screenshot is taken and stored at specified location successfully.", true); 
+					 
+					} 
+					else  
+					{ 
+						Thread.sleep(1000); 
+						if(Page_GlobalSearch.displayNoResultMessage().isDisplayed()) 
+						{ 
+							HelperHand.getscreenshot(helperString); 
+							Reporter.log("Screenshot is taken and stored at specified location successfully.", true); 
+							Reporter.log("No results found for provided string.", true); 
+						} 
+						else 
+						{ 
+							Thread.sleep(1000); 
+							if(Page_GlobalSearch.displayNullResultMessage().isDisplayed()) 
+							{ 
+								HelperHand.getscreenshot(helperString); 
+								Reporter.log("Screenshot is taken and stored at specified location successfully.", true); 
+								Reporter.log("User has not provided any string to search.", true); 
+							} 
+						}	 
+					} 
+				} 
+				catch (AssertionError | NoSuchElementException | TimeoutException e)  
+				{ 
+					e.printStackTrace(); 
+					Assert.fail();	 
 				}
-				catch (AssertionError | NoSuchElementException | TimeoutException e) 
-				{
-					e.printStackTrace();
-					Assert.fail();	
-				}	
-				/*try {
-					if(Page_GlobalSearch.displayNoResultMessage().isDisplayed())
-					{
-						HelperHand.getscreenshot(helperString);
-						Reporter.log("Screenshot is taken and stored at specified location successfully.", true);
-						Reporter.log("No results found for provided string.", true);
-					}
-				} catch (AssertionError |Exception e) {
-					e.printStackTrace();
-					Assert.fail();	
-				}
-				
-				try {
-					if(Page_GlobalSearch.displayNullResultMessage().isDisplayed())
-					{
-						HelperHand.getscreenshot(helperString);
-						Reporter.log("Screenshot is taken and stored at specified location successfully.", true);
-						Reporter.log("User has not provided any string to search.", true);
-					}
-				} catch (AssertionError |Exception e) {
-					e.printStackTrace();
-					Assert.fail();	
-				}	*/
+ 	}
+	*/
+	@Test 
+ 	public void performGlobalSearch1() throws Exception 
+ 	{ 
+ 		helperString = Thread.currentThread().getStackTrace()[1].getMethodName(); 
+ 		Thread.sleep(2000); 
+ 		
+ 		if(Page_GlobalSearch.displayPlaceHolder().isDisplayed()) 
+ 		{
+ 			Page_GlobalSearch.displayPlaceHolder().sendKeys(DataGenerator.prospectEmail); //f176pqvmbs@domain.ins
+			Thread.sleep(1000); 
+			Page_GlobalSearch.displayGoButton().click(); 
+ 		}
+ 		try {
+			Assert.assertTrue(Page_GlobalSearch.validateGlobalSearch(Page_GlobalSearch.getElementText(Page_GlobalSearch.knowErrorMessage())));
+		} catch (AssertionError e) {
+			System.out.println("Global Search failed due to incorrect input or no input.");
+			Assert.fail();
+			e.printStackTrace();
 		}
+ 		
+ 		
+ 		/*(new WebDriverWait(driver, 10)) 
+		.until(ExpectedConditions.visibilityOf(Page_GlobalSearch.displayResultPageText()));
+		Thread.sleep(5000);
+
+		if(Page_GlobalSearch.displayResultPageText().isDisplayed())
+		{
+			if(Page_GlobalSearch.displayNoResultMessage().isDisplayed())
+			{
+				try {
+					Page_GlobalSearch.element = Page_GlobalSearch.displayNoResultMessage();
+					
+					Page_GlobalSearch.validator = Page_GlobalSearch.element.getText();
+				} catch (org.openqa.selenium.NoSuchElementException e) {}
+			}
+			else if(!Page_GlobalSearch.displayNoResultMessage().isDisplayed())
+			{		try {
+				Page_GlobalSearch.element = Page_GlobalSearch.displayNullResultMessage();
+				Page_GlobalSearch.validator =Page_GlobalSearch.element.getText();
+					} catch (org.openqa.selenium.NoSuchElementException e1) {
+						e1.printStackTrace();
+					}
+			}
+			
+		}	*/
+// 		Page_GlobalSearch.findElement();
+ 		
+ 		
+ 		
+ 	/*	switch (Page_GlobalSearch.getElementText(Page_GlobalSearch.element))
+ 		{
+		case "Oops! No data found! Please try again.":
+			Thread.sleep(2000);
+			Page_GlobalSearch.getElementText(Page_GlobalSearch.displayNoResultMessage()).equalsIgnoreCase("Oops! No data found! Please try again.") ;
+			try {	
+				System.out.println("No Result Found");
+			} catch (org.openqa.selenium.NoSuchElementException e) {
+				System.out.println("Exception Handled for No Result.");
+				e.printStackTrace();
+			}
+			break;
+		case "Please Enter Value":
+			Thread.sleep(2000);
+			Page_GlobalSearch.getElementText(Page_GlobalSearch.displayNullResultMessage()).equalsIgnoreCase("Please Enter Value") ;
+			try {
+				System.out.println("No Result Found");
+			} catch (org.openqa.selenium.NoSuchElementException e) {
+				System.out.println("Exception Handled for No Result.");
+				e.printStackTrace();
+			}
+			break;
+			
+		case "Search Result for f176pqvmbs@domain.in":
+			Thread.sleep(2000);
+			try {
+				System.out.println("Result Found");
+			} catch (org.openqa.selenium.NoSuchElementException e) {
+				System.out.println("Exception Handled for Success Result.");
+				e.printStackTrace();
+			}
+			break;
+			
+		/*default:
+			try {
+				System.out.println("Result Found");
+			} catch (org.openqa.selenium.NoSuchElementException e) {
+				System.out.println("Exception Handled for Success Result.");
+				e.printStackTrace();
+			}
+			break;*/
+ 	}
+
 }
+ 	
+ 		
+ 		
+ 		
+ 		/*
+ 		
+ 			try { 
+ 					if(Page_GlobalSearch.displaySuccessResultMessage().isDisplayed()) 
+					{ 
+					
+					System.out.println("Search: "+Page_GlobalSearch.displaySuccessResultMessage().getText());
+//					Page_GlobalSearch.displayPlaceHolder().sendKeys(DataGenerator.prospectEmail); 
+					
+					 
+					(new WebDriverWait(driver, 10)) 
+//				   .until(ExpectedConditions.elementToBeClickable(By.xpath(".//span[contains(text(),'"+DataGenerator.prospectEmail+"')]"))); 
+//					.until(ExpectedConditions.elementToBeClickable(By.xpath(".//span[contains(text(),'f176pqvmbs@domain.in')]"))); 
+					.until(ExpectedConditions.elementToBeClickable(Page_GlobalSearch.displaySuccessResultMessage())); 
+					Thread.sleep(4000);
+					String successResult = driver.findElement(By.xpath(".//span[contains(text(),'f176pqvmbs@domain.in')]")).getText();
+			 		System.out.println("Email: "+successResult);
+//					Assert.assertEquals(DataGenerator.prospectEmail, driver.findElement(By.xpath(".//span[contains(text(),'"+DataGenerator.prospectEmail+"')]")).getText()); 
+					Assert.assertEquals("f176pqvmbs@domain.in", driver.findElement(By.xpath(".//span[contains(text(),'f176pqvmbs@domain.in')]")).getText()); 
+
+					Reporter.log("Specified user details are successfully located in the system.", true); 
+					HelperHand.getscreenshot(helperString);				 
+					Reporter.log("Screenshot is taken and stored at specified location successfully.", true); 
+					} 
+ 				}
+				catch (AssertionError | NoSuchElementException | TimeoutException e)  
+				{ 
+					e.printStackTrace(); 
+					Assert.fail();	 
+				}
+ 				try
+ 				{
+					if(Page_GlobalSearch.displayNoResultMessage().isDisplayed()) 
+					{ 
+						(new WebDriverWait(driver, 10)) 
+						.until(ExpectedConditions.elementToBeClickable(Page_GlobalSearch.displayNoResultMessage())); 
+						HelperHand.getscreenshot(helperString); 
+						Reporter.log("Screenshot is taken and stored at specified location successfully.", true); 
+						Reporter.log("No results found for provided string.", true); 
+					} 
+ 				}
+				catch (AssertionError | NoSuchElementException | TimeoutException e)  
+				{ 
+					e.printStackTrace(); 
+					Assert.fail();	 
+				}
+				try
+				{
+					if(Page_GlobalSearch.displayNullResultMessage().isDisplayed()) 
+					{ 
+						(new WebDriverWait(driver, 10)) 
+						.until(ExpectedConditions.elementToBeClickable(Page_GlobalSearch.displayNullResultMessage())); 
+						HelperHand.getscreenshot(helperString); 
+						Reporter.log("Screenshot is taken and stored at specified location successfully.", true); 
+						Reporter.log("User has not provided any string to search.", true); 
+					} 
+				}
+				catch (AssertionError | NoSuchElementException | TimeoutException e)  
+				{ 
+					e.printStackTrace(); 
+					Assert.fail();	 
+				}*/
+
